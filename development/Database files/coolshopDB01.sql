eb8d175172235f7fea3b99e0cfe45aa9220829e0
m.cainton@gmail.com <m.cainton@gmail.com>
2014-09-14 22:07:18 +1200

    modified sql

--- development/Database files/coolshopDB01.sql
+++ development/Database files/coolshopDB01.sql
@@ -2,7 +2,7 @@ drop database coolshop;
 create database coolshop;
 
 use coolshop;
-
+ -- drop table category;
 create table category (
 category_id int auto_increment primary key,
 category_name varchar(20)
@@ -46,27 +46,27 @@ INSERT INTO Administrator Values(null, 'admin', 'admin');
 
 
 
-insert into category values (1,'Electronics');
-insert into category values (2,'Clothing');
-insert into category values (3,'Music');
-insert into category values (4,'Jewellery');
-insert into category values (5,'Homewares');
-insert into category values (6,'Furniture');
+insert into category values (null,'Electronics');
+insert into category values (null,'Clothing');
+insert into category values (null,'Music');
+insert into category values (null,'Jewellery');
+insert into category values (null,'Homewares');
+insert into category values (null,'Furniture');
 
-insert into product values(2, 1, "Acer Aspire 11.6 Inch Notebook V5-132","The perfect compact Entry-Level Notebook for checking emails, browsing the internet, updating your facebook profile.",
-"images/electronics/acer_01.jpg");
-insert into product values(2, 2, "H&H Boys Spray Jacket","Fabric: Polyester",
-"images/cloths/H_H01.jpg");
+insert into product values(null, 1, "Acer Aspire 11.6 Inch Notebook V5-132","The perfect compact Entry-Level Notebook for checking emails, browsing the internet, updating your facebook profile.",
+"acer_01.jpg");
+insert into product values(null, 2, "H&H Boys Spray Jacket","Fabric: Polyester",
+"H_H01.jpg") ;
 
-insert into product values(4, 3, "100 years of the blues","Pack Size: 2CD",
-"images/music/bluespac_01.jpg");
+insert into product values(null, 3, "100 years of the blues","Pack Size: 2CD",
+"bluespac_01.jpg");
 
-insert into product values(5, 4, "Ane Si Dora Sterling Silver Bracelet 16cm","Start your Ane Si Dora journey with this 16cm sterling silver bracelet. Begin collecting gorgeous charms representing life's beautiful memories! Their great design means that you can easily add new charms as you like.",
-"images/jewellery/Ane_Si_Dora_01 .jpg");
+insert into product values(null, 4, "Ane Si Dora Sterling Silver Bracelet 16cm","Start your Ane Si Dora journey with this 16cm sterling silver bracelet. Begin collecting gorgeous charms representing life's beautiful memories! Their great design means that you can easily add new charms as you like.",
+"Ane_Si_Dora_01 .jpg");
 
-insert into product values(6, 5, "Arctic Flannel Sheet Set Black Queen","Warm Polyester Fleece
+insert into product values(null,5, "Arctic Flannel Sheet Set Black Queen","Warm Polyester Fleece
 No Shrinkage",
-"images/homeware/archtic_flannel.jpg");
-insert into product values(7, 6, "Reside Espresso Coffee Table Rectangular","Warm Polyester FleeceDecorate your home in style with the \"Reside Espresso\" range exclusive to The Warehouse. Combining the latest in contemporary styling and cool chocolate / coffee colour \"Reside Espresso\" is the latest in modern home fashion. 
+"archtic_flannel.jpg");
+insert into product values(null, 6, "Reside Espresso Coffee Table Rectangular","Warm Polyester FleeceDecorate your home in style with the \"Reside Espresso\" range exclusive to The Warehouse. Combining the latest in contemporary styling and cool chocolate / coffee colour \"Reside Espresso\" is the latest in modern home fashion. 
 No Shrinkage",
-"images/furniture/espresso_coffee_table_01.jpg");
+"espresso_coffee_table_01.jpg");
