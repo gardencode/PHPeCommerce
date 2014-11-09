03583bd9279ed34bf7b4573502fce4ae8482cbd3
Dr Michael Lopez <mike.lopez@acm.org>
2014-11-10 09:56:43 +1300

    Completed products model (should be OK, but not yet tested) unit test to follow this afternoon (going to software team meeting now)

--- site/models/ProductsModel.php
+++ site/models/ProductsModel.php
@@ -1,16 +1,106 @@
 <?php
+/*
+	Created by Mike
+	Based on Cainton's model and some of Glen's ideas
+	
+	This class manages a list of products
+	
+	Usage is
+		1) create model
+		2) set filters
+		3) get product list
+	
+	Filters can be set for
+		category id
+		word match in name
+		price range (or just min or max price)
+		limit (and offset) for rows returned
+			... this allows paging through products
+			
+			
+	Note that a search can be refined by setting additional filters and retrieving
+    the product list again	
+*/
+
 class ProductsModel extends AbstractModel {
 
     private $products;
+    private $constraints;
+	private $limit;
 
     public function __construct($db) {
         parent::__construct($db);
-        $this->products = array();
-        $this->load();
+        $this->products = null;
+		$this->constraints = array();
+		$limit=null;
+	}
+	
+	public function setCategoryFilter ($categoryId) {
+		if (!ctype_digit($categoryId)) {
+			throw new InvalidDataException('Invalid category ID');
+		}
+		$this->constraints[]="categoryId = $categoryId";
+		$this->products=null;
+	}
+	
+	public function setNameMatch ($text) {
+		$safeText = $this->getDB()->escape($text);
+		$this->constraints[]="name like '%$safeText%'";
+		$this->products=null;
+	}
+
+	public function setPriceRange ($minimum=null, $maximum=null) {
+		if ($minimum === null && $maximum === null) {
+			return;
+		}
+		if ($minimum !==null && !is_numeric ($minimum)) {
+			throw new InvalidDataException('Invalid minimum value');
+		}
+		if ($maximum !==null && !is_numeric ($maximum)) {
+			throw new InvalidDataException('Invalid maximum value');
+		}
+		if ($minimum !==null && $maximum !== null) {
+			$this->constraints[]="price between $minimum and $maximum";
+		} else if ($minimum !==null) {
+			$this->constraints[]="price >= $minimum";
+		} else {
+			$this->constraints[]="price <= $maximum";
+		}
+		$this->products=null;
+	}
+
+	public function setLimit ($limit, $offset=null) {
+		if (!is_numeric ($limit)) {
+			throw new InvalidDataException('Invalid limit value');
+		}
+		if ($offset !==null && !is_numeric ($offset)) {
+			throw new InvalidDataException('Invalid offset value');
+		}
+		if ($offset === null ) {
+			$this->limit = " LIMIT $limit";
+		} else {
+			$this->limit = " LIMIT $limit OFFSET $offset";
+		}
+		$this->products=null;
+	}
+	
+    public function getProducts() {
+		if ($this->products===null) {
+			$this->products = array();
+			$this->load();
+		}
+        return $this->products;
     }
 
     private function load() {
-        $sql = "select id, categoryId, name, description, price, image from product;";
+        $sql = "select id, categoryId, name, description, price, image from product";
+		if (count($this->constraints) > 0) {
+			$sql .= $this->whereClause();
+		}
+		$sql .= " order by name asc";
+		if ($this->limit !==null) {
+			$sql .= $this->limit;
+		}
         $rows = $this->getDB()->query($sql);
         foreach ($rows as $row){
             $productId = $row['id'];
@@ -23,9 +113,8 @@ class ProductsModel extends AbstractModel {
             $this->products[]=$product;
         }
     }
-
-    public function getProducts() {
-        return $this->products;
-    }
+	private function whereClause() {
+		return ' where (' . implode (') AND (',$this->constraints).')';
+	}
 }
 ?>
