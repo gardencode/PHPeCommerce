63a4ca894104dc258ebe1d7c6918a4cdfc86f9b6
haydenmclaren <hayden_mclaren@hotmail.com>
2014-11-20 22:54:11 +1300

    Updated orders and order lines

--- site/models/OrderModel.php
+++ site/models/OrderModel.php
@@ -1,31 +1,33 @@
 <?php
-//todo ASSERTIONS
+
 class OrderModel extends AbstractEntityModel {
-    // instance data
+
     private $streetNumber;
     private $streetName;
     private $city;
     private $postCode;
-    private $totalPrice;
-    // standard constructor
+    private $orderDate;
+    private $dateSent;
+    private $trackingNumber;
+
     public function __construct($db, $orderId = null) {
         parent::__construct($db, $orderId);
     }
-    // NB constructor with many parameters changed to factory pattern below
-    public static function createFromFields(IDatabase $db,$orderId,$streetNumber,$streetName,$city,$postCode, $totalPrice) {
+
+    public static function createFromFields(IDatabase $db,$orderId,$streetNumber,$streetName,$city,$postCode, $orderDate, $dateSent, $trackingNumber) {
         $model = new OrderModel ($db);
         $model->setID($orderId);
         $model->setStreetNumber($streetNumber);
         $model->setStreetName($streetName);
         $model->setcity($city);
         $model->setPostCode($postCode);
-        $model->setTotalPrice($totalPrice);
+        $model->setOrderDate($orderDate);
+        $model->setDateSent($dateSent);
+        $model->setTrackingNumber($trackingNumber);
         $model->didChange(false);
         return $model;
     }
-    /*
-        Getters of private data
-    */
+
     public function getStreetNumber(){
         return $this->streetNumber;
     }
@@ -38,120 +40,141 @@ class OrderModel extends AbstractEntityModel {
     public function getPostCode(){
         return $this->postCode;
     }
-    public function getTotalPrice(){
-        return $this->totalPrice;
+    public function getOrderDate(){
+        return $this->orderDate;
     }
-    /*
-        Setters of private data (all have validators)
-    */
+    public function getDateSent(){
+        return $this->dateSent;
+    }
+    public function getTrackingNumber(){
+        return $this->trackingNumber;
+    }
+
     public function setStreetNumber($value){
-//        $this->assertNoError($this->errorInCategoryId($this->getDB(),$value));
+        $this->assertNoError($this->errorInStreetNumber($value));
         $this->streetNumber = $value;
         $this->didChange();
     }
     public function setStreetName($value){
-//        $this->assertNoError($this->errorInName($value));
+        $this->assertNoError($this->errorInStreetName($value));
         $this->streetName = $value;
         $this->didChange();
     }
     public function setCity($value){
-//        $this->assertNoError($this->errorInDescription($value));
+        $this->assertNoError($this->errorInCity($value));
         $this->city = $value;
         $this->didChange();
     }
     public function setPostCode($value){
-//        $this->assertNoError($this->errorInPrice($value));
+        $this->assertNoError($this->errorInPostCode($value));
         $this->postCode = $value;
         $this->didChange();
     }
-    public function setTotalPrice($value){
-//        $this->assertNoError($this->errorInPrice($value));
-        $this->totalPrice = $value;
+    public function setOrderDate($value){
+        //  $this->assertNoError($this->errorInOrderDate($value));
+        $this->orderDate = $value;
         $this->didChange();
     }
-    /* 		==============
-            must overrides
-            ==============
-    */
-    // 	set default values for instance data
-    // (required fields should be set to null)
+    public function setDateSent($value){
+        //  $this->assertNoError($this->errorInDateSent($value));
+        $this->dateSent = $value;
+        $this->didChange();
+    }
+    public function setTrackingNumber($value){
+        $this->assertNoError($this->errorInTrackingNumber($value));
+        $this->trackingNumber = $value;
+        $this->didChange();
+    }
+
     protected function init() {
         $this->streetNumber=null;
         $this->streetName=null;
         $this->city=null;
         $this->postCode=null;
-        $this->totalPrice=null;
+        $this->orderDate=null;
+        $this->dateSent=null;
+        $this->trackingNumber=null;
     }
-    // load instance data from database
+
     protected function loadData($row) {
         $this->streetNumber=$row['streetNumber'];
         $this->streetName=$row['streetName'];
         $this->city=$row['city'];
         $this->postCode=$row['postCode'];
-        $this->totalPrice=$row['totalPrice'];
+        $this->orderDate=$row['orderDate'];
+        $this->dateSent=$row['dateSent'];
+        $this->trackingNumber=$row['trackingNumber'];
     }
-    // return false if any required field is null
+
     protected function allRequiredFieldsArePresent() {
         return $this->streetNumber !== null &&
         $this->streetName !== null &&
         $this->city !== null &&
-        $this->postCode !== null &&
-        $this->totalPrice !== null;
+        $this->postCode !== null;
     }
-    // load instance data from database
+
     protected function getLoadSql($orderId) {
-        return 	"select streetNumber, streetName, city, postCode, totalPrice, from orders where id = $orderId";
+        return 	"select streetNumber, streetName, city, postCode, orderDate, dateSent, trackingNumber, from orders where id = $orderId";
     }
-    // sql to insert instance data into database
+
     protected function getInsertionSql() {
         $streetNumber = $this->safeSqlNumber($this->streetNumber);
         $streetName = $this->safeSqlString($this->streetName);
         $city = $this->safeSqlString($this->city);
         $postCode = $this->safeSqlString($this->postCode);
-        $totalPrice = $this->safeSqlNumber($this->totalPrice);
-        return "insert into orders(streetNumber, streetName, city, postCode, totalPrice) ".
-        "values ($streetNumber, $streetName, $city, $postCode, $totalPrice)";
+        $dateSent = $this->safeSqlDate($this->dateSent);
+        $trackingNumber = $this->safeSqlNumber($this->trackingNumber);
+        return "insert into orders(streetNumber, streetName, city, postCode, orderDate, dateSent, trackingNumber) ".
+        "values ($streetNumber, $streetName, $city, $postCode, CURDATE(), $dateSent, $trackingNumber)";
     }
-    // sql to update database record from instance data
+
     protected function getUpdateSql() {
         $streetNumber = $this->safeSqlNumber($this->streetNumber);
         $streetName = $this->safeSqlString($this->streetName);
         $city = $this->safeSqlString($this->city);
         $postCode = $this->safeSqlString($this->postCode);
-        $totalPrice = $this->safeSqlNumber($this->totalPrice);
+        $orderDate = $this->safeSqlDate($this->orderDate);
+        $dateSent = $this->safeSqlDate($this->dateSent);
+        $trackingNumber = $this->safeSqlNumber($this->trackingNumber);
         return "update orders set ".
         "streetNumber=$streetNumber, ".
         "streetName=$streetName, ".
         "city=$city, ".
         "postCode=$postCode, ".
-        "totalPrice=$totalPrice, ".
-        "where id=".$this->getId();
+        "orderDate=$orderDate, ".
+        "dateSent=$dateSent, ".
+        "trackingNumber=$trackingNumber, ".
+        "where id=".$this->getID();
     }
-    // sql to delete this instance
+
     protected function getDeletionSql() {
         return 'delete from orders where id = '.$this->getId();
     }
-    /*
-        Static validation routines
-    */
-    public static function errorInCategoryId(IDatabase $db, $value) {
-        if (CategoryModel::isExistingId($db, $value)) {
-            return null;
-        }
-        return "Invalid category ID ($value)";
+
+    public static function errorInStreetNumber($value) {
+        return self::errorInRequiredField('Street Number',$value,4);
     }
-    public static function errorInName($value) {
-        return self::errorInRequiredField('Product name',$value,50);
+    public static function errorInStreetName($value) {
+        return self::errorInRequiredField('Street Name',$value,32);
     }
-    public static function errorInDescription($value) {
-        return self::errorInRequiredField('Description',$value,300);
+    public static function errorInCity($value) {
+        return self::errorInRequiredField('City',$value,32);
     }
-    public static function errorInPrice($value) {
-        return self::errorInRequiredNumericField('Price', $value, 2, 0.00, 99999999.99);
+    public static function errorInPostCode($value) {
+        return self::errorInRequiredField('Post Code',$value,4);
     }
-    public static function errorInImage($value) {
-        return self::errorInRequiredField('Image',$value,100);
+    //todo
+    public static function errorInOrderDate($value) {
+        return self::errorInRequiredField('Order Date',$value,4);
     }
+    //todo
+    public static function errorInDateSent($value) {
+        return self::errorInRequiredField('Date Sent',$value,4);
+    }
+    public static function errorInTrackingNumber($value) {
+        return self::errorInRequiredField('Tracking Number',$value,20);
+    }
+
     public static function isExistingId($db,$id) {
         return self::checkExistingId($db,$id,
             'select 1 from product where id='.$id);
